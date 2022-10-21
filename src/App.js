4f3c1b044efca8e385f302e53ae62026ae451f0d
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-10-21 11:40:44 +0530

    created build folder

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ function App() {
 
   return (
     <div className='app'>
-        
         <div className='container'>
             <h2 className='center'>BMI Calculator</h2>
             <form onSubmit={calcBmi}>
