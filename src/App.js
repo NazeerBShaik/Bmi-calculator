aa8fe34b54c4bd1026e0ed4494a92f857b977e25
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-10-21 08:57:57 +0530

    some test

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
 
   return (
     <div className='app'>
+        
         <div className='container'>
             <h2 className='center'>BMI Calculator</h2>
             <form onSubmit={calcBmi}>
